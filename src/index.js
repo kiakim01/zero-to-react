979afb8c7da6a468995b52531b524cea836667b7
kiakim01 <hnmpot0318@naver.com>
2026-02-11 19:56:13 +0900

    Init repository

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// src/index.js
+const arrowFunction = () => {
+  return 'Hello, Babel!';
+};
+
+const promiseExample = new Promise((resolve, reject) => {
+  setTimeout(() => resolve('Promise resolved!'), 1000);
+});
+
+promiseExample.then(console.log);
+
+console.log(arrowFunction());
