f8a413d002c4dbea1a153de5179ce6540c63053f
kiakim01 <hnmpot0318@naver.com>
2026-02-20 22:34:00 +0900

    chore - webpack 설정 파일 불필요한 주석 제거

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,6 @@
-const { merge } = require('webpack-merge');
-const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
-const common = require('./webpack.common.js');
+const { merge } = require('webpack-merge')
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
   mode: 'development',
@@ -43,20 +43,4 @@ module.exports = merge(common, {
   optimization: {
     runtimeChunk: 'single',
   },
-});
-
-/*
- * Hot Reload vs HMR (Hot Module Replacement) 차이점:
- * 
- * 1. Hot Reload:
- *    - 파일이 변경되면 전체 페이지를 새로고침
- *    - 애플리케이션의 상태(state)가 초기화됨
- *    - 간단하지만 개발 중 상태를 잃어버림
- * 
- * 2. HMR (Hot Module Replacement):
- *    - 변경된 모듈만 교체하여 업데이트
- *    - 애플리케이션의 상태를 유지함
- *    - React Refresh는 React 컴포넌트를 위한 HMR 구현
- *    - 컴포넌트 수정 시 상태를 유지하면서 UI만 업데이트
- *    - 더 빠르고 효율적인 개발 경험 제공
- */
+})
